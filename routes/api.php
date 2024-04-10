b1d45b2e8d679f672ef4e3090ee9109164fd660a
SoufianeNajim <soufianenajim100@gmail.com>
2024-04-10 09:26:44 +0000

    update

--- routes/api.php
+++ routes/api.php
@@ -55,5 +55,5 @@ Route::get('/user', function () {
 Route::post('register',[AuthController::class,'register']);
 Route::post('login', [AuthController::class,'login']);
 Route::post('refresh', [AuthController::class,'refresh']);
-Route::post('update', [AuthController::class,'update']);
+Route::patch('update', [AuthController::class,'update']);
 Route::get('logout', [AuthController::class,'logout']);
